abc14ea3cbd4e161f51023dbf62024f5bdad2325
zuokanyq <zuokanyq@gmail.com>
2016-12-15 17:34:04 +0800

    未完成的中间版本

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,11 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
+import { ActivatedRoute,Params} from '@angular/router';
+import { Location } from '@angular/common';
+
 import {Hero} from './hero'
+import { HeroService } from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector : 'my-hero-detail',
@@ -12,12 +18,20 @@ import {Hero} from './hero'
         <input [(ngModel)]="hero.name" placeholder="name"/>
       </div>
     </div>
-    `
+    `,
+  providers: [HeroService]
 })
-export class HeroDetailComponent  { 
+export class HeroDetailComponent implements OnInit { 
 
   @Input()
   hero: Hero;
+
+ constructor(
+    private heroService:HeroService,
+    private route:ActivatedRoute,
+    private location:Location
+    
+  ){}
 
   onSelect(hero: Hero): void {
   this.hero = hero;
